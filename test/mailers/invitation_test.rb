bf1ea565dabcbe72642f8f439100838f2a845f82
WendyBeth <wendybeth010@gmail.com>
2015-03-05 09:23:57 -0800

    link to invitation preview within admin panel. this is not something that will work in production yet but I'm leaving it since it's not member-facing and it is a good placeholder for an idea that may or may not be useful.

--- test/mailers/invitation_test.rb
+++ test/mailers/invitation_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class InvitationTest < ActionMailer::TestCase
-  test "leader_invitation" do 
+  test "leader_invitation" do
+    # this test needs more information
+    # specifically: email.from, email.subject
+    # and content placed in fixture leader_invitation
+    # for email.body
+
     email = Invitation.leader_invitation('me@example.com').deliver
 
     assert_not ActionMailer::Base.deliveries.empty?
