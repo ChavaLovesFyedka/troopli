f59926bda769ce21d611d3f7f40c3f993f659cb6
WendyBeth <wendybeth010@gmail.com>
2015-03-10 23:07:32 -0400

    add some sample data to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,38 @@
 # badgeparser1.badge_list
 
 #Badges 
+
+members = Member.create([  # MEMBER #
+                         { email: "member@example.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           role: "member" },
+                           # LEADER # 
+                         { email: "leader@example.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           role: "leader" },
+                           # ADMIN #
+                         { email: "admin@example.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           role: "admin" }
+                        ])
+                        
+troop = Troop.create(name: "Lilypads",
+                     description: "We love to camp!",
+                     age_level: 12,
+                     leader_id: Member.find_by(email: "leader@example.com").id)
+
+events = Event.create([
+                       { name: "Party",
+                         description: "At Red Lobster",
+                         start_at: Time.now,
+                         end_at: Time.now,
+                         troop_id: Troop.first.id },
+                       { name: "Camping",
+                         description: "In the woods",
+                         start_at: Time.now,
+                         end_at: Time.now,
+                         troop_id: Troop.first.id }
+                      ])
