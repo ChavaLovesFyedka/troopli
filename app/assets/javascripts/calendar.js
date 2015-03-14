59e6afbc89b0c731e1be33187a1631d0d97f2a91
WendyBeth <wendybeth010@gmail.com>
2015-03-14 00:30:57 -0400

    move calendar to top of page

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,11 +1,11 @@
 $(function() {
   $('.fa-calendar').click(function() {
-    if ($("#toggle-stats").css("display") == "none")
-    {      
-      $("html, body").animate({
-        scrollTop: $("#anchor").offset().top
-      }, 1000);
-    }
+//    if ($("#toggle-stats").css("display") == "none")
+//    {      
+//      $("html, body").animate({
+//        scrollTop: $("#anchor").offset().top
+//      }, 1000);
+//    }
 
     $("#toggle-stats").slideToggle("slow", function() {
     });
