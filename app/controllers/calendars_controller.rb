668ae472ac751e98f76603269a927777d1a5ef27
WendyBeth <wendybeth010@gmail.com>
2015-03-16 11:18:47 -0400

    add events properly to calendars. only events that belong to the troop show up on the troop's calendar. events only show up on the day specified. another feature might include being able to specify on an event whether it takes place daily/weekly/monthly/yearly etc.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,7 +1,7 @@
 class CalendarsController < ApplicationController
   def show 
-    @troop = Troop.find(params[:troop_id])
+    @troop = Troop.includes(:events).find(params[:troop_id])
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @events_by_date = Event.order(:start_at)
+    @events_by_date = @troop.events.order(:start_at)
   end
 end
