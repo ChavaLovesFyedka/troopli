7d1bde07762b5635bcfa6eb35c54e2d1efb18f59
WendyBeth <wendybeth010@gmail.com>
2015-03-17 23:43:44 -0400

    style .today class so Date.today is highlighted on the calendar. Restrict height of calendar lists so days with a lot of events don't break the calendar. still need to think of a way to see all events for a day if it has more events than can fit in the calendar...

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,11 @@ class Event < ActiveRecord::Base
   scope :next_3, -> { where("start_at > ?", Time.now)
                       .order(start_at: :asc)
                       .limit(3) }
+
+  def is_on?(date)
+    self.start_at.day == date.day &&
+    self.start_at.month == date.month &&
+    self.start_at.year == date.year
+  end
+
 end
